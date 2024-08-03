feca19ed37fcdeb0a566ddaa5a30781589d51656
Pdubey1994 <committeeurban@gmail.com>
2024-08-03 20:39:36 +0530

    Initial commit

---
+++ src/main/java/com/urban/committee/UcPaymentServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.urban.committee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcPaymentServiceAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UcPaymentServiceAppApplication.class, args);
+    }
+
+}
