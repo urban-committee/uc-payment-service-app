1545f92a24dd5ee83d5e20e10209a1ce6c8c985c
Pdubey1994 <committeeurban@gmail.com>
2024-08-04 15:58:04 +0530

    Eureka Registry
    Eureka Registry

--- src/main/java/com/urban/committee/UcPaymentServiceAppApplication.java
+++ src/main/java/com/urban/committee/UcPaymentServiceAppApplication.java
@@ -2,8 +2,10 @@ package com.urban.committee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UcPaymentServiceAppApplication {
 
     public static void main(String[] args) {
